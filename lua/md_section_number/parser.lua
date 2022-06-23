e0750b15e14fc6934e49ae5f0646ea404555e263
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2022-06-23 18:51:52 +0800

    chore: complete core function

--- lua/md_section_number/parser.lua
+++ lua/md_section_number/parser.lua
@@ -1,16 +1,73 @@
-local PairParser = {}
-PairParser.__index = PairParser
-PairParser.pairs = {}
+local Stack = require("md_section_number.stack")
+local M = {}
 
-function PairParser:add_pairs(startPair, endPair)
-	table.insert(self, { startPair, endPair })
+M.ignore_pairs = {
+  { "```", "```" },
+  { "\\~\\~\\~", "\\~\\~\\~" },
+  { "<!--", "-->" },
+}
+
+M.heading_pattern = "^#+ "
+
+M.max_level = 4
+
+function M.add_pairs(startPair, endPair)
+  table.insert(M.ignore_pairs, { startPair, endPair })
 end
 
-function PairParser:parse_pairs(line)
-	-- todo
+local function judgeHeadingLine(line)
+  local s, e = string.find(line, M.heading_pattern)
+  local length = s and e - s + 1 or 0
+  return nil ~= s, length
 end
 
-PairParser:add_pairs("```", "```")
-PairParser:add_pairs("<!--", "-->")
+function M.get_heading_lines(all_lines)
+  if nil == all_lines or #all_lines == 0 then
+    return {}
+  end
+  local heading_lines = {}
+  local stack = Stack:new({})
+  -- find ignore section by ignore_pairs
+  for line_index, line in ipairs(all_lines) do
+    for pair_index, pair in ipairs(M.ignore_pairs) do
+      local start_pair_location = vim.fn.match(line, pair[1])
+      local end_pair_location = vim.fn.match(line, pair[2], start_pair_location + 1)
+      if pair[1] == pair[2] then
+        if stack:is_empty() then
+          if -1 ~= start_pair_location and -1 == end_pair_location then
+            print(pair_index)
+            stack:push(pair_index)
+          end
+        else
+          if -1 ~= start_pair_location then
+            if pair_index == stack:peek() then
+              stack:pop()
+            else
+              stack:push(pair_index)
+            end
+          end
+        end
+      end
+      if pair[1] ~= pair[2] then
+        if -1 ~= start_pair_location and -1 == end_pair_location then
+          stack:push(pair_index)
+        end
+        if -1 == start_pair_location and -1 ~= end_pair_location then
+          if pair_index == stack:peek() then
+            stack:pop()
+          end
+        end
+      end
+    end
+    print(stack:is_empty())
+    if stack:is_empty() then
+      local is_heading, length = judgeHeadingLine(line)
+      if is_heading then
+        table.insert(heading_lines, { line_index, line, length })
+      end
+    end
+  end
+  return heading_lines
+end
 
-return PairParser
+return M
--- lua/md_section_number/stack.lua
+++ lua/md_section_number/stack.lua
@@ -1,20 +1,37 @@
 local Stack = {}
 
 Stack.__index = Stack
+Stack.elements = {}
 
 function Stack:push(element)
-	table.insert(self, #self + 1, element)
+  table.insert(self.elements, self:length() + 1, element)
+end
+
+function Stack:length()
+  return #self.elements
 end
 
 function Stack:pop()
-	local element = self[#self]
-	table.remove(self, #self)
-	return element
+  if self:is_empty() then
+    return nil
+  end
+  local element = self.elements[self:length()]
+  table.remove(self.elements, self:length())
+  return element
 end
 
-function Stack:new(attrs)
-	attrs = attrs or {}
-	return setmetatable(attrs, self)
+function Stack:new(elements, attrs)
+  attrs = attrs or {}
+  attrs.elements = elements or {}
+  return setmetatable(attrs, self)
+end
+
+function Stack:is_empty()
+  return self:length() == 0
+end
+
+function Stack:peek()
+  return self.elements[self:length()]
 end
 
 return Stack
