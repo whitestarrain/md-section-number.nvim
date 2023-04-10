84600f9ba414576382c420461e7681e770e5d903
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-04-10 21:38:36 +0800

    refactor: ui refactor

--- lua/md_section_number/ui.lua
+++ lua/md_section_number/ui.lua
@@ -9,6 +9,20 @@ M.options = {
   position = "right",
   indent_space_number = 2,
   header_prefix = "- ",
+  winopts = {
+    number = false,
+    relativenumber = false,
+    wrap = false,
+    equalalways = false,
+    winfixwidth = true,
+    list = false,
+    spell = false,
+  },
+  bufopts = {
+    bufhidden = "wipe",
+    filetype = "tagbar",
+    modifiable = false,
+  },
 }
 
 function M.setup(opts)
@@ -16,7 +30,7 @@ function M.setup(opts)
   M.options.position = opts.position or M.options.position
 end
 
-M.global = {
+M.viewBind = {
   MdBufNumber = nil,
   MdHeaders = nil,
   TocWin = nil,
@@ -28,13 +42,13 @@ M.global = {
 local bindBufGroup = vim.api.nvim_create_augroup("MdBindBufAutoCmd", {})
 
 function M.unbind()
-  if M.global.BindBuf then
+  if M.viewBind.BindBuf then
     vim.api.nvim_clear_autocmds({
       group = bindBufGroup,
-      buffer = M.global.BindBuf,
+      buffer = M.viewBind.BindBuf,
     })
   end
-  M.global = {}
+  M.viewBind = {}
 end
 
 local move_tbl = {
@@ -50,16 +64,12 @@ local function create_side_window()
   vim.api.nvim_win_set_width(win, M.options.width)
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_win_set_buf(win, buf)
-  vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-  vim.api.nvim_buf_set_option(buf, "filetype", "tagbar")
-  vim.api.nvim_buf_set_option(buf, "modifiable", false)
-  vim.opt_local["number"] = false
-  vim.opt_local["relativenumber"] = false
-  vim.opt_local["wrap"] = false
-  vim.opt_local["equalalways"] = false
-  vim.opt_local["winfixwidth"] = true
-  vim.opt_local["list"] = false
-  vim.opt_local["spell"] = false
+  for k,v in pairs(M.options.bufopts) do
+    vim.api.nvim_buf_set_option(buf, k, v)
+  end
+  for k, v in pairs(M.options.winopts) do
+    vim.opt_local[k] = v
+  end
   return win, buf
 end
 
@@ -74,35 +84,36 @@ local function add_indent_for_header(header)
 end
 
 local function reload_headers()
-  if not M.global.BindBuf then
+  if not M.viewBind.BindBuf then
     return
   end
-  M.global.MdHeaders = parser.get_heading_lines(vim.api.nvim_buf_get_lines(M.global.BindBuf, 0, -1, false))
+  M.viewBind.MdHeaders = parser.get_heading_lines(vim.api.nvim_buf_get_lines(M.viewBind.BindBuf, 0, -1, false))
 end
 
 local function render_headers()
-  vim.api.nvim_buf_set_option(M.global.TocBuf, "modifiable", true)
+  vim.api.nvim_buf_set_option(M.viewBind.TocBuf, "modifiable", true)
   reload_headers()
   -- get header text
   local all_headers_with_indent = {}
-  for _, header in ipairs(M.global.MdHeaders) do
+  for _, header in ipairs(M.viewBind.MdHeaders) do
     table.insert(all_headers_with_indent, add_indent_for_header(header))
   end
   -- clear
-  vim.api.nvim_buf_set_lines(M.global.TocBuf, 0, -1, false, {})
+  vim.api.nvim_buf_set_lines(M.viewBind.TocBuf, 0, -1, false, {})
   -- reset
-  vim.api.nvim_buf_set_lines(M.global.TocBuf, 0, -1, false, all_headers_with_indent)
-  vim.api.nvim_buf_set_option(M.global.TocBuf, "modifiable", false)
+  vim.api.nvim_buf_set_lines(M.viewBind.TocBuf, 0, -1, false, all_headers_with_indent)
+  vim.api.nvim_buf_set_option(M.viewBind.TocBuf, "modifiable", false)
 end
 
 function M.jump_header()
-  if not M.global.MdHeaders then
+  if not M.viewBind.MdHeaders then
     reload_headers()
   end
-  local header_index = math.min(vim.api.nvim_win_get_cursor(0)[1], #M.global.MdHeaders)
-  local line_number = math.min(M.global.MdHeaders[header_index][1] + 1, vim.api.nvim_buf_line_count(M.global.BindBuf))
-  vim.api.nvim_set_current_win(M.global.BindWin)
-  vim.api.nvim_win_set_cursor(M.global.BindWin, { line_number, 0 })
+  local header_index = math.min(vim.api.nvim_win_get_cursor(0)[1], #M.viewBind.MdHeaders)
+  local line_number =
+    math.min(M.viewBind.MdHeaders[header_index][1] + 1, vim.api.nvim_buf_line_count(M.viewBind.BindBuf))
+  vim.api.nvim_set_current_win(M.viewBind.BindWin)
+  vim.api.nvim_win_set_cursor(M.viewBind.BindWin, { line_number, 0 })
   vim.api.nvim_feedkeys("zz", "n", false)
 end
 
@@ -118,32 +129,32 @@ local function set_mappings()
   local mappings = {
     ["<cr>"] = M.jump_header,
     q = function()
-      M.close(M.global.TocWin)
+      M.close(M.viewBind.TocWin)
     end,
     r = function()
-      local origin_position = vim.api.nvim_win_get_cursor(M.global.TocWin)
+      local origin_position = vim.api.nvim_win_get_cursor(M.viewBind.TocWin)
       render_headers()
-      local new_line_number = vim.api.nvim_buf_line_count(M.global.TocBuf)
+      local new_line_number = vim.api.nvim_buf_line_count(M.viewBind.TocBuf)
       if new_line_number == 0 then
         return
       end
       local row = math.min(new_line_number, origin_position[1])
-      local line = vim.api.nvim_buf_get_lines(M.global.TocBuf, row - 1, row, false)[1]
+      local line = vim.api.nvim_buf_get_lines(M.viewBind.TocBuf, row - 1, row, false)[1]
       if line == nil then
         line = ""
       end
       local col = math.min(#line, origin_position[2])
-      vim.api.nvim_win_set_cursor(M.global.TocWin, { row, col })
+      vim.api.nvim_win_set_cursor(M.viewBind.TocWin, { row, col })
     end,
   }
   for k, v in pairs(mappings) do
-    vim.keymap.set("n", k, v, { buffer = M.global.TocBuf, silent = true, noremap = true })
+    vim.keymap.set("n", k, v, { buffer = M.viewBind.TocBuf, silent = true, noremap = true })
   end
 end
 
 local function set_autocmd()
   vim.api.nvim_create_autocmd("WinClosed", {
-    buffer = M.global.TocBuf,
+    buffer = M.viewBind.TocBuf,
     callback = function()
       M.unbind()
     end,
@@ -153,12 +164,12 @@ local function set_autocmd()
 end
 
 function M.open_side_window()
-  M.global.BindBuf = vim.api.nvim_get_current_buf()
-  M.global.BindWin = vim.api.nvim_get_current_win()
+  M.viewBind.BindBuf = vim.api.nvim_get_current_buf()
+  M.viewBind.BindWin = vim.api.nvim_get_current_win()
 
   local win, buf = create_side_window()
-  M.global.TocWin = win
-  M.global.TocBuf = buf
+  M.viewBind.TocWin = win
+  M.viewBind.TocBuf = buf
 
   render_headers()
   set_mappings()
@@ -166,8 +177,8 @@ function M.open_side_window()
 end
 
 function M.toggle()
-  if M.global.TocWin then
-    M.close(M.global.TocWin)
+  if M.viewBind.TocWin then
+    M.close(M.viewBind.TocWin)
   else
     M.open_side_window()
   end
